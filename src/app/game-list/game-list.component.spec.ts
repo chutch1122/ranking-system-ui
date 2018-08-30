333f46f18465043af420329904531a39c5c06438
brachels <BRACHELS@vizientinc.com>
2018-08-30 08:10:40 -0500

    more tests - BR

--- src/app/game-list/game-list.component.spec.ts
+++ src/app/game-list/game-list.component.spec.ts
@@ -1,6 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { GameListComponent } from './game-list.component';
+import {GameListComponent} from './game-list.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Player} from '../models/player.model';
+import {Rating} from '../models/rating.model';
+import {GameType} from '../models/game-type.model';
 
 describe('GameListComponent', () => {
   let component: GameListComponent;
@@ -8,7 +12,8 @@ describe('GameListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameListComponent ]
+      declarations: [ GameListComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -21,5 +26,46 @@ describe('GameListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getFullName()', ()  => {
+    it('should concat first name and last name', () => {
+      const player: Player = {
+        id: 1,
+        firstName: 'Bob',
+        lastName: 'McFoosball',
+        ratings: []
+      };
+
+      const name = component.getFullName(player);
+
+      const expected = 'Bob M.';
+
+      expect(name).toEqual(expected);
+    });
+  });
+
+  describe('getRatingDelta', () => {
+    it('should return a players delta', () => {
+
+
+      const rating: Rating = {
+        game: GameType.FOOSBALL,
+        rating: 1000,
+        delta: 9,
+        createdOn: ''
+      };
+
+      const player: Player = {
+        id: 1,
+        firstName: 'Bob',
+        lastName: 'McFoosball',
+        ratings: [rating[0]]
+      };
+
+      const delta = component.getRatingDelta(player);
+
+
+    });
   });
 });
